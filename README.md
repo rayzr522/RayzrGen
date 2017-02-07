14cbd239c48987721f52a36fece55b5717face14
Rayzr522 <rayzr522@gmail.com>
2017-02-07 10:41:47 -0500

    Project 'RayzrGen' generated
    Generated using CNLPluginArchetype and custom CLI project generator tool by Rayzr

---
+++ README.md
@@ -0,0 +1,4 @@
+# RayzrGen
+A random custom world generator that I made for fun.
+
+*Note: This README is a pre-generated stub*
---
+++ src/main/java/com/rayzr522/rayzrgen/RayzrGen.java
@@ -0,0 +1,15 @@
+package com.rayzr522.rayzrgen;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author Rayzr
+ */
+public class RayzrGen extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
